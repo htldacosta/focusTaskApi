f163ab0265e1f581d0648cb539086569108c4e25
htldacosta <hitaludcosta@gmail.com>
2026-05-10 14:04:38 -0400

    class service was finalized

--- src/main/java/com/focustask/service/TaskService.java
+++ src/main/java/com/focustask/service/TaskService.java
@@ -1,7 +1,11 @@
 package com.focustask.service;
 
+import com.focustask.entity.Task;
 import com.focustask.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskService {
@@ -11,4 +15,25 @@ public class TaskService {
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
+    public Task createTask(Task task) {
+
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(UUID id) {
+
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Tarefa nao encontrada no banco!"));
+
+    }
+
+    public void deleteTask(UUID id){
+        Task task = getTaskById(id);
+        taskRepository.delete(task);
+    }
 }
+
